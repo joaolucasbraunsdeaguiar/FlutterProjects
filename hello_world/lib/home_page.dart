18eec0372f8d1863d238bd582fec66c72b65a2c0
João Lucas <joaolucasbraunsa@gmail.com>
2022-01-12 11:59:55 -0300

    Primeiro Commit

---
+++ hello_world/lib/home_page.dart
@@ -0,0 +1,34 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Você clicou: $counter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ hello_world/lib/myapp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'Home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
